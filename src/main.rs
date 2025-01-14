93f4d1864330bd86467934a86cbc47c6f22541fa
mochi <pe86ki@gmail.com>
2025-01-14 22:19:21 +0800

    stufs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+use std::sync::Arc;
 use askama::Template;
 use axum::{response::Html, routing::get, Router};
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(handler));
+        .nest_service("/public", ServeDir::new("./public"))
+        .route("/", get(index));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
@@ -13,13 +16,18 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
+const MOTDS: [&str; 1] = ["meow :3"];
+
 #[derive(Template)]
 #[template(path = "index.html")]
 struct IndexTemplate {
+    motd: &'static str
 }
 
-async fn handler() -> Html<Box<str>> {
-    let template = IndexTemplate {};
-    
+async fn index() -> Html<Box<str>> {
+    let template = IndexTemplate {
+        motd: MOTDS[0],
+    };
+
     Html(template.render().unwrap().into())
 }
