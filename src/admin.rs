e7c399245e5e130e5b8fa70e86a8130e4a88ac18
mochi <pe86ki@gmail.com>
2025-01-19 17:57:02 +0800

    motd editor and updown

--- src/admin.rs
+++ src/admin.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 use askama::Template;
-use axum::extract::Query;
+use axum::extract::{Path, Query};
 use axum::http::{HeaderMap, StatusCode};
 use axum::response::{IntoResponse, Response};
 use axum::Router;
@@ -14,9 +14,13 @@ use subtle::ConstantTimeEq;
 pub fn routes() -> Router {
     Router::new()
         .route("/", get(admin))
-        .route("/add-motd", get(add_motd))
         .route("/set-likes", get(set_likes))
         .route("/set-working-on", get(set_working_on))
+        .route("/motd/new", get(add_motd))
+        .route("/motd/{id}/edit", get(edit_motd))
+        .route("/motd/{id}/delete", get(delete_motd))
+        .route("/motd/{id}/up", get(motd_up))
+        .route("/motd/{id}/down", get(motd_down))
 }
 
 const UNAUTHORIZED_BASIC: (StatusCode, [(&str, &str); 1]) =
@@ -39,7 +43,8 @@ macro_rules! basic_auth {
 #[template(path = "admin.html")]
 struct AdminTemplate {
     likes: Arc<str>,
-    working_on: Arc<str>
+    working_on: Arc<str>,
+    motds: Vec<Arc<str>>
 }
 
 async fn admin(
@@ -51,27 +56,8 @@ async fn admin(
     askama_axum::IntoResponse::into_response(AdminTemplate {
         likes: messages.likes.clone(),
         working_on: messages.working_on.clone(),
+        motds: messages.motds.clone()
     }).into_response()
-}
-
-#[derive(Deserialize)]
-struct AddMotd {
-    motd: Arc<str>
-}
-
-async fn add_motd(
-    headers: HeaderMap,
-    Query(add_motd): Query<AddMotd>
-) -> Response {
-    basic_auth!(headers);
-    let mut messages = MESSAGES.write().await;
-
-    messages.motds.push(add_motd.motd);
-    if let Err(e) = write_toml("./messages.toml", &*messages) {
-        return (StatusCode::INTERNAL_SERVER_ERROR, format!("couldnt write into messages.toml: {e}")).into_response();
-    }
-
-    (StatusCode::SEE_OTHER, [("Location", "/admin")]).into_response()
 }
 
 #[derive(Deserialize)]
@@ -107,6 +93,92 @@ async fn set_working_on(
     let mut messages = MESSAGES.write().await;
 
     messages.working_on = set_working_on.working_on;
+    if let Err(e) = write_toml("./messages.toml", &*messages) {
+        return (StatusCode::INTERNAL_SERVER_ERROR, format!("couldnt write into messages.toml: {e}")).into_response();
+    }
+
+    (StatusCode::SEE_OTHER, [("Location", "/admin")]).into_response()
+}
+
+#[derive(Deserialize)]
+struct AddMotd {
+    motd: Arc<str>
+}
+
+async fn add_motd(
+    headers: HeaderMap,
+    Query(add_motd): Query<AddMotd>
+) -> Response {
+    basic_auth!(headers);
+    let mut messages = MESSAGES.write().await;
+
+    messages.motds.push(add_motd.motd);
+    if let Err(e) = write_toml("./messages.toml", &*messages) {
+        return (StatusCode::INTERNAL_SERVER_ERROR, format!("couldnt write into messages.toml: {e}")).into_response();
+    }
+
+    (StatusCode::SEE_OTHER, [("Location", "/admin")]).into_response()
+}
+
+#[derive(Deserialize)]
+struct EditMotd {
+    motd: Arc<str>
+}
+
+async fn edit_motd(
+    headers: HeaderMap,
+    Path(id): Path<usize>,
+    Query(edit_motd): Query<EditMotd>
+) -> Response {
+    basic_auth!(headers);
+    let mut messages = MESSAGES.write().await;
+
+    messages.motds[id] = edit_motd.motd;
+    if let Err(e) = write_toml("./messages.toml", &*messages) {
+        return (StatusCode::INTERNAL_SERVER_ERROR, format!("couldnt write into messages.toml: {e}")).into_response();
+    }
+
+    (StatusCode::SEE_OTHER, [("Location", "/admin")]).into_response()
+}
+
+async fn delete_motd(
+    headers: HeaderMap,
+    Path(id): Path<usize>,
+) -> Response {
+    basic_auth!(headers);
+    let mut messages = MESSAGES.write().await;
+
+    messages.motds.remove(id);
+    if let Err(e) = write_toml("./messages.toml", &*messages) {
+        return (StatusCode::INTERNAL_SERVER_ERROR, format!("couldnt write into messages.toml: {e}")).into_response();
+    }
+
+    (StatusCode::SEE_OTHER, [("Location", "/admin")]).into_response()
+}
+
+async fn motd_up(
+    headers: HeaderMap,
+    Path(id): Path<usize>,
+) -> Response {
+    basic_auth!(headers);
+    let mut messages = MESSAGES.write().await;
+
+    messages.motds.swap(id, id + 1);
+    if let Err(e) = write_toml("./messages.toml", &*messages) {
+        return (StatusCode::INTERNAL_SERVER_ERROR, format!("couldnt write into messages.toml: {e}")).into_response();
+    }
+
+    (StatusCode::SEE_OTHER, [("Location", "/admin")]).into_response()
+}
+
+async fn motd_down(
+    headers: HeaderMap,
+    Path(id): Path<usize>,
+) -> Response {
+    basic_auth!(headers);
+    let mut messages = MESSAGES.write().await;
+
+    messages.motds.swap(id, id - 1);
     if let Err(e) = write_toml("./messages.toml", &*messages) {
         return (StatusCode::INTERNAL_SERVER_ERROR, format!("couldnt write into messages.toml: {e}")).into_response();
     }
